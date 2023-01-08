4127804f26d26977c1dad2c53a20bdc76872978d
Julien Clément <julienclement22@gmail.com>
2023-01-08 13:40:59 +0100

    Implement logging

--- API/app/main.py
+++ API/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import app.helpers.logging_helper as logging_helper
+from app.controllers.health_controller import health_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+app.include_router(health_router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,3 +16,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+logging_helper.set_up_logging()
