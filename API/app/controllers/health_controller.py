1b1c951bc77b114d3107baefdc9eecf8ca37ff89
Julien Clément <julienclement22@gmail.com>
2023-01-08 13:42:52 +0100

    Implement health check

---
+++ API/app/controllers/health_controller.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Response
+
+from app.helpers.logging_helper import LoggingRoute
+
+health_router = APIRouter(
+    prefix='/health',
+    tags=['health'],
+    responses={404: {'description': 'Not found'}},
+    route_class=LoggingRoute
+)
+
+
+@health_router.get("/")
+async def get_health_status(response: Response) -> str:
+    response.status_code = 200
+    return 'Ok'
