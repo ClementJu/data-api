fd99647017b389b6e35924d64c8857da19aefdd6
Julien Clément <julienclement22@gmail.com>
2023-01-09 20:26:27 +0100

    Implement tests for GET method getting dialog data + Improve test base by adding helper functions to get data, post dialog data and post consent + Refactor accordingly

--- API/tests/test_consent.py
+++ API/tests/test_consent.py
@@ -1,7 +1,4 @@
-from typing import Dict
-
-from starlette.responses import Response
-from tests.helpers import test_base, test_client
+from tests.helpers import test_base
 
 from app.data.entities import ConsentEntity, DialogDataEntity, TemporaryDialogDataEntity
 
@@ -10,14 +7,9 @@ def test_save_consent_should_work() -> None:
     test_base.empty_database()
 
     # Case True
-    _insert_dialog_data(dialog_id='did34')
+    test_base.insert_dialog_data(dialog_id='did34')
+    test_base.give_consent(has_given_consent=True, dialog_id='did34')
 
-    test_client.post(
-        '/consents/did34',
-        json={
-            'has_given_consent': 'true'
-        }
-    )
     db = test_base.get_database()
     consents = db.query(ConsentEntity).all()
     assert len(consents) == 1
@@ -30,14 +22,8 @@ def test_save_consent_should_work() -> None:
     assert consents_for_given_dialog_id[0].dialog_id == 'did34'
 
     # Case False
-    _insert_dialog_data(dialog_id='did55')
-
-    test_client.post(
-        '/consents/did55',
-        json={
-            'has_given_consent': 'false'
-        }
-    )
+    test_base.insert_dialog_data(dialog_id='did55')
+    test_base.give_consent(has_given_consent=False, dialog_id='did55')
 
     consents_after_two_insertions = db.query(ConsentEntity).all()
     assert len(consents_after_two_insertions) == 2
@@ -50,42 +36,22 @@ def test_save_consent_should_work() -> None:
 
 def test_save_consent_should_throw_if_no_temporary_data_for_dialog_id() -> None:
     test_base.empty_database()
-    response = test_client.post(
-        '/consents/did55',
-        json={
-            'has_given_consent': 'true'
-        }
-    )
+    response = test_base.give_consent(has_given_consent=True, dialog_id='did55')
     assert response.status_code == 404
 
 
 def test_save_consent_should_throw_if_consent_was_already_given() -> None:
     test_base.empty_database()
 
-    _insert_dialog_data(dialog_id='did55')
+    test_base.insert_dialog_data(dialog_id='did55')
 
-    response = test_client.post(
-        '/consents/did55',
-        json={
-            'has_given_consent': 'true'
-        }
-    )
+    response = test_base.give_consent(has_given_consent=True, dialog_id='did55')
     assert response.status_code == 200
 
-    response = test_client.post(
-        '/consents/did55',
-        json={
-            'has_given_consent': 'true'
-        }
-    )
+    response = test_base.give_consent(has_given_consent=True, dialog_id='did55')
     assert response.status_code == 409
 
-    response = test_client.post(
-        '/consents/did55',
-        json={
-            'has_given_consent': 'false'
-        }
-    )
+    response = test_base.give_consent(has_given_consent=False, dialog_id='did55')
     assert response.status_code == 409
 
 
@@ -93,18 +59,13 @@ def test_save_consent_should_move_data_if_consent_is_given() -> None:
     test_base.empty_database()
     db = test_base.get_database()
 
-    _insert_dialog_data(dialog_id='did55')
+    test_base.insert_dialog_data(dialog_id='did55')
 
     assert len(db.query(ConsentEntity).all()) == 0
     assert len(db.query(TemporaryDialogDataEntity).all()) == 1
     assert len(db.query(DialogDataEntity).all()) == 0
 
-    response = test_client.post(
-        '/consents/did55',
-        json={
-            'has_given_consent': 'true'
-        }
-    )
+    response = test_base.give_consent(has_given_consent=True, dialog_id='did55')
     assert response.status_code == 200
 
     assert len(db.query(ConsentEntity).all()) == 1
@@ -115,7 +76,7 @@ def test_save_consent_should_move_data_if_consent_is_given() -> None:
 def test_save_consent_should_erase_data_if_consent_is_not_given() -> None:
     test_base.empty_database()
 
-    _insert_dialog_data(dialog_id='did55')
+    test_base.insert_dialog_data(dialog_id='did55')
 
     db = test_base.get_database()
 
@@ -123,27 +84,9 @@ def test_save_consent_should_erase_data_if_consent_is_not_given() -> None:
     assert len(db.query(TemporaryDialogDataEntity).all()) == 1
     assert len(db.query(DialogDataEntity).all()) == 0
 
-    response = test_client.post(
-        '/consents/did55',
-        json={
-            'has_given_consent': 'false'
-        }
-    )
+    response = test_base.give_consent(has_given_consent=False, dialog_id='did55')
     assert response.status_code == 200
 
     assert len(db.query(ConsentEntity).all()) == 1
     assert len(db.query(TemporaryDialogDataEntity).all()) == 0
     assert len(db.query(DialogDataEntity).all()) == 0
-
-
-def _get_first_dialog_data_payload() -> Dict[str, str]:
-    dialog_data_payloads = test_base.get_test_payloads_save_dialog_data()
-    return dialog_data_payloads[0]
-
-
-def _insert_dialog_data(payload: Dict[str, str] = None, customer_id: str = 'id12',
-                        dialog_id: str = 'did55') -> Response:
-    return test_client.post(
-        f'/data/{customer_id}/{dialog_id}',
-        json=_get_first_dialog_data_payload() if payload is None else payload
-    )
