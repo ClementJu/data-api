756885d447bf12c86bf7410ccf213678dece13d1
Julien Clément <julienclement22@gmail.com>
2023-01-09 20:32:29 +0100

    Implement GET endpoint and method that retrieves data for which consent was given based on a query and returns the data sorted by most recent first

--- API/app/controllers/data_controller.py
+++ API/app/controllers/data_controller.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -18,3 +20,9 @@ data_router = APIRouter(
 def save_dialog_data(dialog_data: DialogDataCreateModel, customer_id: str,
                      dialog_id: str, db: Session = Depends(get_db)) -> DialogDataModel:
     return service.save_dialog_data(dialog_data=dialog_data, customer_id=customer_id, dialog_id=dialog_id, db=db)
+
+
+@data_router.get("", response_model=List[DialogDataModel])
+def get_dialog_data(language: Optional[str] = None, customer_id: Optional[str] = None,
+                    db: Session = Depends(get_db)) -> List[DialogDataModel]:
+    return service.get_dialog_data(language=language, customer_id=customer_id, db=db)
--- API/app/services/data_service.py
+++ API/app/services/data_service.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from typing import List, Optional
 
 from sqlalchemy.orm import Session
 
-from app.data.entities import TemporaryDialogDataEntity
+from app.data.entities import DialogDataEntity, TemporaryDialogDataEntity
 from app.data.models import DialogDataCreateModel, DialogDataModel
 
 
@@ -19,3 +20,16 @@ def save_dialog_data(dialog_data: DialogDataCreateModel, customer_id: str,
     db.commit()
     db.refresh(dialog_data_to_insert)
     return dialog_data_to_insert
+
+
+def get_dialog_data(language: Optional[str], customer_id: Optional[str], db: Session) -> List[DialogDataModel]:
+    query_builder = db.query(DialogDataEntity)
+
+    if language is not None and language != '':
+        query_builder = query_builder.filter(DialogDataEntity.language == language.lower())
+
+    if customer_id is not None and customer_id != '':
+        query_builder = query_builder.filter(DialogDataEntity.customer_id == customer_id)
+
+    matching_data = query_builder.order_by(DialogDataEntity.received_at_timestamp_utc.desc()).all()
+    return matching_data
